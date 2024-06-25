b17121565354d4a409f98d7f35eb5770290cc468
rajkumar <rajkumar@gmail.com>
2024-06-25 15:11:19 +0000

    file

---
+++ TERRAFORM/vpc_module/variable.tf
@@ -0,0 +1,12 @@
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+variable "my_subnet1" {
+  default = "10.0.1.0/24"
+}
+variable "my_subnet2" {
+  default = "10.0.2.0/24"
+}
+variable "envirolment" {
+  
+}
