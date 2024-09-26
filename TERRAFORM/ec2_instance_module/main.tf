1d4a2308f42825483d2b42274ae0746489644507
CHANDANAGIRIRAJKUMAR <153308295+CHANDANAGIRIRAJKUMAR@users.noreply.github.com>
2024-09-26 23:39:45 +0530

    Update main.tf

--- TERRAFORM/ec2_instance_module/main.tf
+++ TERRAFORM/ec2_instance_module/main.tf
@@ -5,9 +5,13 @@ resource "aws_instance" "web_instance" {
   ami           = var.web_ami_id
   instance_type = var.web_instance_type
   user_data = data.template_file.userdata.rendered
-
+  subnet_id = var.subnet_id
+  security_groups = [var.security_group]
   tags = {
-    Name = "${var.enviornment}-serber" #de/prod server (commonds are terraform apply -var-file dev.tsvars)
+    Name = "${var.enviornment}-server" #de/prod server (commonds are terraform apply -var-file dev.tsvars)
   }
 }
+
+
+
 
